c95fcb8f9ebe43b063efa04896df7abe0683ffda
Andrew_F <de5ertf0x.yandex.ru>
2021-10-17 20:36:07 +0300

    Сделаны задания 2 3 и 4 2. Создал макет с несколькими EditText, где использовать атрибут inputType: text, textPersonName, phone, number, textPassword,textEmailAddress + Тажже добавил переход на новую активити по кнопке Registration + Содержимое всех строк перенес в ресурсы + Поменял цвет ActionBar 3. Добавить в проект элемент CalendarView. + Добавил ScrollView Для корректного просмотра 4. * Разобраться, что такое параметр ems. Я так понял - что ems задает ширину TextView, чтобы соответствовать тексту из n ' m ' букв независимо от фактического расширения текста и размера текста.
    но только тогда, когда layout_width установлено значение "wrap_content"
    
    То есть при значении 3 ширина textView бк=удет соответствовать ширине 3 букв m. Для других языков берется ширина самой широкой буквы
    
    Применил ems с параметром 6 на @+id/text2

--- app/src/main/java/com/example/android_lesson_01/MainActivity.java
+++ app/src/main/java/com/example/android_lesson_01/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.android_lesson_01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegistrationActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+
 }
